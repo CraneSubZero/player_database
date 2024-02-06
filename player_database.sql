f311b97cb87b5d934e3a4baec5f2b2b53d3a4c4d
BERBENZANA <120699750@comlabserver.csabcomlab.edu.ph>
2024-02-06 14:34:45 +0800

    add localhost folder

---
+++ player_database.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 06, 2024 at 07:32 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_database`
+--
+
+CREATE TABLE `player_database` (
+  `ID` varchar(255) NOT NULL,
+  `FIRSTNAME` varchar(255) NOT NULL,
+  `LASTNAME` varchar(255) NOT NULL,
+  `GAMENAME` varchar(255) NOT NULL,
+  `IGN` varchar(255) NOT NULL,
+  `POSITION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
