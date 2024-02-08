8afd4d6704d13c260750f2a31afbb7930d4c73ec
BERBENZANA <120699750@comlabserver.csabcomlab.edu.ph>
2024-02-08 13:34:31 +0800

    adjusted the margins

--- index.php
+++ index.php
@@ -74,6 +74,7 @@
             <th scope="col">IGN</th>
             <th scope="col">Position</th>
           </tr>
+          
         </thead>
         <tbody>
 
@@ -96,21 +97,14 @@
       </table>s
     </div>
 
-
   </div>
-
-
-
-
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
 
-
 </body>
 
 <script>
-
 
   function delete_student(id) {
       if (confirm("Do you want to delete this data? ")){
@@ -118,7 +112,5 @@
       }
   }
 
-      
 </script>
-
 </html>
