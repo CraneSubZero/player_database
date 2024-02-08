6c261a80a46fbbd6987f5373fd6d1f6b526ccbc0
Mae Ann Pacquiao <maepacquiao0405@gmail.com>
2024-02-08 10:09:13 +0800

    a_i for Id added

--- player_database.sql
+++ player_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2024 at 02:54 PM
+-- Generation Time: Feb 08, 2024 at 03:01 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,6 +35,26 @@ CREATE TABLE `player_database` (
   `Ign` varchar(255) NOT NULL,
   `Position` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `player_database`
+--
+ALTER TABLE `player_database`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `player_database`
+--
+ALTER TABLE `player_database`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
